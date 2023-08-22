711832097469360c3ab9a6068d6874a844668b2d
Jarle <jarlesyv@hiof.no>
2023-08-22 09:32:18 +0200

    Actual sorting this time.

--- addnumbers.js
+++ addnumbers.js
@@ -3,9 +3,11 @@ function sort() {
     const valueStore = document.querySelectorAll('.container')[0];
     const valueBoxes = Array.from(valueStore.children);
     const sortedValues = [];
+
     valueBoxes.forEach((input) => {
         sortedValues.push(input.value);
     })
+
     sortedValues.sort(function(a, b) {return a - b;});
 
     console.log(sortedValues);
